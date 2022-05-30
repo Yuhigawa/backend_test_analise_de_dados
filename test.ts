5370e16e91d6bb5a94a348cf3b58997b2516180b
yuhigawa <pablostreiski@outlook.com>
2022-05-30 15:46:29 -0300

    del: removed unnecessary file

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { try_catch_file_sync } from './src/readfileSync';
-
-(async() => {
-    let compras_data = await try_catch_file_sync('./src/data/compras.json');
-    let vendas_data = await try_catch_file_sync('./src/data/vendas.json');
-
-    console.log( 'foi' );
-    console.log( vendas_data );
-})();
